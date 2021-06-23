626587619e6d4cf83d4ecfd85e081cbe77640a96
gvillaverde <gonvillaverde@gmail.com>
2021-06-23 12:35:04 -0300

    Solved the fizzbuzz puzzle

--- ex1.js
+++ ex1.js
@@ -11,16 +11,29 @@ let counter = 0;
     console.log(tag);
 }*/
 
-for (; counter <= 50; counter = counter + 1) {
+/*for (; counter <= 50; counter = counter + 1) {
     if (counter <= 25) {
         tag = tag + "#";
-        console.log(tag);
     }
     if (counter >= 26) {
         tag = tag.slice(0, -1);
-        console.log(tag);
     }
     
-}
+    console.log(tag);
+}*/
 
 // Ex 2
+
+for(counter = 1; counter <= 100; counter = counter + 1) {
+    let countBy3 = counter % 3;
+    let countBy5 = counter % 5;
+    if (countBy3 == 0){
+        console.log("fizz");
+    }
+    if (countBy5 == 0) {
+        console.log("buzz");
+    }
+
+    console.log(counter);
+}
+
