424e7734566f6059a7d5b380366959fb974dbbea
gvillaverde <gonvillaverde@gmail.com>
2021-06-24 11:18:04 -0300

    Segundo ejercicio

--- ex1.js
+++ ex1.js
@@ -2,7 +2,7 @@
 
 
 // Ex 1
-let tag = "";
+let tag = "#";
 let counter = 0;
 
 /*while (counter <= 7) {
@@ -24,7 +24,7 @@ let counter = 0;
 
 // Ex 2
 
-for(counter = 1; counter <= 100; counter = counter + 1) {
+/*for(counter = 1; counter <= 100; counter = counter + 1) {
     let countBy3 = counter % 3;
     let countBy5 = counter % 5;
     if (countBy3 == 0){
@@ -35,5 +35,7 @@ for(counter = 1; counter <= 100; counter = counter + 1) {
     }
 
     console.log(counter);
-}
+}*/
+
+// Ex 3 
 
